cdc3090acfb435fc29ed199cd3ed530ee658a523
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 19:43:22 +0100

    Update LanguageSwitcher.vue

--- examples/vue-example/src/components/LanguageSwitcher.vue
+++ examples/vue-example/src/components/LanguageSwitcher.vue
@@ -1,1 +1,18 @@
-placholder
+<template>
+  <div>
+    <button @click="setLanguage('en')">English</button>
+    <button @click="setLanguage('fr')">Français</button>
+  </div>
+</template>
+
+<script>
+import { useI18n } from 'rosettajs-i18n';
+
+export default {
+  name: 'LanguageSwitcher',
+  setup() {
+    const { setLanguage } = useI18n();
+    return { setLanguage };
+  }
+}
+</script>
