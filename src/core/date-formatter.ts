ece7ff88f3415dcbb63deb5cab170cec969445fc
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 20:37:25 +0100

    Update date-formatter.ts

--- src/core/date-formatter.ts
+++ src/core/date-formatter.ts
@@ -1,1 +1,3 @@
-placeholder
+export function formatDate(date: Date, language: string, options?: Intl.DateTimeFormatOptions): string {
+  return new Intl.DateTimeFormat(language, options).format(date);
+}
