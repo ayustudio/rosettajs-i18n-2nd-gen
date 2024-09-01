25147e5ec58de6533c5eced512053b33330b3040
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 20:34:18 +0100

    Update number-formatter.ts

--- src/core/number-formatter.ts
+++ src/core/number-formatter.ts
@@ -1,1 +1,3 @@
-placeholder
+export function formatNumber(number: number, language: string, options?: Intl.NumberFormatOptions): string {
+  return new Intl.NumberFormat(language, options).format(number);
+}
