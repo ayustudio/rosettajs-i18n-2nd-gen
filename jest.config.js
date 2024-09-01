8a9bde0a28eabe5bb1d2a66343f97cdc8f2981d1
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 19:21:47 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
