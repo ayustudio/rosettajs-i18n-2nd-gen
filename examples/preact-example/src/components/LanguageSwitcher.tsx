e9f17ed564facb5e4915e60923333076be4ba0c0
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 20:07:07 +0100

    Update LanguageSwitcher.tsx

--- examples/preact-example/src/components/LanguageSwitcher.tsx
+++ examples/preact-example/src/components/LanguageSwitcher.tsx
@@ -1,1 +1,13 @@
-placeholder
+import { h } from 'preact';
+import { usePreactTranslation } from 'rosettajs-i18n';
+
+export default function LanguageSwitcher() {
+  const { setLanguage } = usePreactTranslation();
+
+  return (
+    <div>
+      <button onClick={() => setLanguage('en')}>English</button>
+      <button onClick={() => setLanguage('fr')}>Français</button>
+    </div>
+  );
+}
