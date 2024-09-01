52778a0b039750208f974fd0abb179070c251876
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 20:15:36 +0100

    Update plugin-manager.ts

--- src/utils/plugin-manager.ts
+++ src/utils/plugin-manager.ts
@@ -1,1 +1,18 @@
-placeholder
+import { Translator } from '../core/translator';
+
+export interface Plugin {
+  name: string;
+  init: (translator: Translator) => void;
+}
+
+export class PluginManager {
+  private plugins: Plugin[] = [];
+
+  registerPlugin(plugin: Plugin): void {
+    this.plugins.push(plugin);
+  }
+
+  initPlugins(translator: Translator): void {
+    this.plugins.forEach(plugin => plugin.init(translator));
+  }
+}
