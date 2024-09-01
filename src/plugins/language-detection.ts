f7e0a1501f1944ae4acdadbae5c65f12582e653b
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 20:25:45 +0100

    Update language-detection.ts

--- src/plugins/language-detection.ts
+++ src/plugins/language-detection.ts
@@ -1,1 +1,19 @@
-placeholder
+import { Translator } from '../core/translator';
+
+export function languageDetectionPlugin() {
+  return {
+    name: 'languageDetection',
+    init: (translator: Translator) => {
+      translator.detectLanguage = () => {
+        const languages = navigator.languages || [navigator.language];
+        for (const lang of languages) {
+          const shortLang = lang.split('-')[0];
+          if (translator.hasTranslation(shortLang)) {
+            return shortLang;
+          }
+        }
+        return translator.getDefaultLanguage();
+      };
+    }
+  };
+}
