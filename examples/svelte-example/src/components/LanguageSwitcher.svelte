476fbec62f209226222637a322edcf54f850da74
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 19:51:38 +0100

    Update LanguageSwitcher.svelte

--- examples/svelte-example/src/components/LanguageSwitcher.svelte
+++ examples/svelte-example/src/components/LanguageSwitcher.svelte
@@ -1,1 +1,10 @@
-placeholder
+<script>
+import { getTranslator } from 'rosettajs-i18n';
+
+const { setLanguage } = getTranslator();
+</script>
+
+<div>
+  <button on:click={() => setLanguage('en')}>English</button>
+  <button on:click={() => setLanguage('fr')}>Français</button>
+</div>
